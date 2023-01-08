4ac789246509cdb41c15398982cd56921a7536bb
Pedro "Spark" Fernandes <pedrofernandesspark@gmail.com>
2023-01-08 12:42:16 -0300

    add NICK and PRIVMSG

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 
+users = {}
+ids = {}
+
 # Create a socket
 # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
 # socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
@@ -36,57 +39,42 @@ def receive_message(client_socket):
 
     try:
 
-        # Receive our "header" containing message length, it's size is defined and constant
         message_header = client_socket.recv(HEADER_LENGTH)
 
-        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
         if not len(message_header):
             return False
 
-        # Convert header to int value
         message_length = int(message_header.decode('utf-8').strip())
 
-        # Return an object of message header and message data
         return {'header': message_header, 'data': client_socket.recv(message_length)}
-
+        
     except:
-
-        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
-        # or just lost his connection
-        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
-        # and that's also a cause when we receive an empty message
         return False
 
 while True:
 
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-
-    # Iterate over notified sockets
     for notified_socket in read_sockets:
 
-        # If notified socket is a server socket - new connection, accept it
         if notified_socket == server_socket:
-
 
             client_socket, client_address = server_socket.accept()
 
-            # Client should send his name right away, receive it
             user = receive_message(client_socket)
 
-            # If False - client disconnected before he sent his name
             if user is False:
                 continue
 
-            # Add accepted socket to select.select() list
             sockets_list.append(client_socket)
 
-            # Also save username and username header
             clients[client_socket] = user
 
             print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+            users[user['data'].decode('utf-8')] = user['data'].decode('utf-8')
+            ids[user['data'].decode('utf-8')] = client_socket
 
-        # Else existing socket is sending a message
+
         else:
 
             # Receive message
@@ -106,16 +94,40 @@ while True:
 
             # Get user by notified socket, so we will know who sent the message
             user = clients[notified_socket]
+            if  'NICK' in message["data"].decode("utf-8"):
+                if users.get(message["data"].decode("utf-8")[5:]) is None :
+                    users[user["data"].decode("utf-8")] = message["data"].decode("utf-8")[5:]
+                    print(users)
+                else :
+                    print('Error canot user that username')
 
-            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+            if  'PRIVMSG' in message["data"].decode("utf-8"):
+                msg = message["data"].decode("utf-8")[8:]
+                msg = msg.split()
+                alvo = msg[0]
+                mensagem = msg[1]
+                idDoAlvo = list(users.keys())[list(users.values()).index(alvo)]
+                print(f'Received private message from {users[user["data"].decode("utf-8")]}: {mensagem} para o usuario {idDoAlvo}')
+                mensagem = mensagem.encode("utf-8")
+                # Iterate over connected clients and broadcast message
+                for client_socket in clients:
 
-            # Iterate over connected clients and broadcast message
-            for client_socket in clients:
+                    # But don't sent it to sender
+                    if client_socket == ids[idDoAlvo]:
+                        print(f"{len(mensagem):<{HEADER_LENGTH}}".encode('utf-8'))
+                        client_socket.send(user['header'] + users[user["data"].decode("utf-8")].encode("utf-8") + f"{len(mensagem):<{HEADER_LENGTH}}".encode('utf-8') + mensagem)
 
-                # But don't sent it to sender
-                if client_socket != notified_socket:
+            else :
+                # print(ids)
+                print(f'Received message from {users[user["data"].decode("utf-8")]}: {message["data"].decode("utf-8")}')
 
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+                # Iterate over connected clients and broadcast message
+                for client_socket in clients:
+
+                    # But don't sent it to sender
+                    if client_socket != notified_socket:
+
+                        client_socket.send(user['header'] + users[user["data"].decode("utf-8")].encode("utf-8") + message['header'] + message['data'])
 
     # It's not really necessary to have this, but will handle some socket exceptions just in case
     for notified_socket in exception_sockets:
